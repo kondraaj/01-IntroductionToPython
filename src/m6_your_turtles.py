5881f37db20e384d3aae2d80188ae132c7e0508a
kondraaj <kondraaj@rose-hulman.edu>
2018-03-05 09:48:43 -0500

    all done

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Aaron Kondrat.
 """
 ###############################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ###############################################################################
 
 ###############################################################################
-# TODO: 2.
+# DONE: 2.
 #   You should have RUN the  m4e_loopy_turtles  module and READ its code.
 #   (Do so now if you have not already done so.)
 #
@@ -27,3 +27,39 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 ###############################################################################
+
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+pepperoni = rg.SimpleTurtle()
+pepperoni.pen = rg.Pen('black', 5)
+pepperoni.speed = 35
+
+size = 90
+
+for k in range(16):
+    pepperoni.draw_circle(size)
+    pepperoni.pen_up()
+    pepperoni.left(37)
+    pepperoni.forward(7)
+    pepperoni.right(12)
+    pepperoni.pen_down()
+    size = size + 10
+
+sausage = rg.SimpleTurtle()
+sausage.pen = rg.Pen('blue', 2)
+sausage.speed = 20
+
+size_1 = 90
+
+for x in range(17):
+    sausage.draw_square(size_1)
+    sausage.pen_up()
+    sausage.right(45)
+    sausage.forward(7)
+    sausage.right(50)
+    sausage.pen_down()
+    size_1 = size_1 - 5
+
+window.close_on_mouse_click()
