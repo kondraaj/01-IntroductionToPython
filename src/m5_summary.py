95adf2d606c3182eab73573aada25277be67606b
kondraaj <kondraaj@rose-hulman.edu>
2018-03-05 09:32:15 -0500

    all done

--- src/m5_summary.py
+++ src/m5_summary.py
@@ -2,15 +2,34 @@
 An exercise that summarizes what you have learned in this Session.
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Aaron Kondrat.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+zebra = rg.SimpleTurtle('turtle')
+zebra.pen = rg.Pen('red', 15)
+zebra.speed = 40
+zebra.forward(60)
+zebra.right(37)
+zebra.forward(55)
+zebra.right(37)
+zebra.forward(50)
+zebra.right(37)
+zebra.forward(45)
+zebra.right(37)
+zebra.forward(40)
+zebra.right(37)
+zebra.forward(35)
+
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #   Write code that constructs a SimpleTurtle with a red Pen
 #   and makes it move around a bit.  Don't forget to:
 #     -- import rosegraphics and construct a TurtleWindow
@@ -27,3 +46,4 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #   As always, COMMIT-and-PUSH when you are done with this module.
 #
 ###############################################################################
+window.close_on_mouse_click()
